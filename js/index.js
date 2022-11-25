c136a7ee6dc7a4af6d827557f792aa27eaec7cb4
cristian <crisvini>
2022-11-25 14:59:53 -0300

    adição de font awesome, bootstrap criação de função head

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    head("buyIt")
+})
+
+function head(title) {
+    var template = `<meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+                    <script src="https://kit.fontawesome.com/60656bf09a.js" crossorigin="anonymous"></script>
+                    <link href="../lib/fontawesome-free-6.2.1-web/css/fontawesome.css" rel="stylesheet">
+                    <link href="../lib/fontawesome-free-6.2.1-web/css/brands.css" rel="stylesheet">
+                    <link href="../lib/fontawesome-free-6.2.1-web/css/solid.css" rel="stylesheet">
+                    <title>${title}</title>`
+    document.head.innerHTML += template
+}
